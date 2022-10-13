003d64f87a2b977aa49c7964fb924f6a24e2567c
Michelle Markl <65341595+MMischi@users.noreply.github.com>
2022-10-13 12:53:59 +0200

    Fix styling

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -18,4 +18,40 @@ export class HomePage {
   };
 
   constructor() {}
+
+  learningKit = [];
+  learningKits = [
+    {
+      id: 1,
+      name: 'Geschichte',
+    },
+    {
+      id: 2,
+      name: 'Informatik',
+    },
+    {
+      id: 3,
+      name: 'Marketing',
+    },
+  ];
+
+  ngOnInit() {
+    console.log(this.learningKit);
+  }
+
+  compareWith(o1, o2) {
+    if (!o1 || !o2) {
+      return o1 === o2;
+    }
+
+    if (Array.isArray(o2)) {
+      return o2.some((o) => o.id === o1.id);
+    }
+
+    return o1.id === o2.id;
+  }
+
+  handleChange(ev) {
+    this.learningKit = ev.target.value;
+  }
 }
