6f6a71363c5747a6d7e8ce6021f9647db157d746
Michelle Markl <65341595+MMischi@users.noreply.github.com>
2022-11-30 14:25:16 +0100

    feat: load studykits from local storage

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -25,15 +25,17 @@ export class HomePage {
 
   ngOnInit() { }
 
+  async ionViewWillEnter() {
+    this.studykits = await this.service.getAllStudykits();
+  }
+
   compareWith(o1, o2) {
     if (!o1 || !o2) {
       return o1 === o2;
     }
-
     if (Array.isArray(o2)) {
       return o2.some((o) => o.id === o1.id);
     }
-
     return o1.id === o2.id;
   }
 
