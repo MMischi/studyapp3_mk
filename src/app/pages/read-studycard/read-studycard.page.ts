dcf668d808f515205e87ec8fa9334df89696570a
Michelle Markl <65341595+MMischi@users.noreply.github.com>
2022-12-16 18:50:44 +0100

    refactor: for better readability, ... (read-studycard)

--- src/app/pages/read-studycard/read-studycard.page.ts
+++ src/app/pages/read-studycard/read-studycard.page.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { ToastController } from "@ionic/angular";
-import { DataService } from "src/app/services/data.service";
-import { Card } from "src/app/services/_interfaces/card";
-import { Studykit } from "src/app/services/_interfaces/studykit";
+
+import {
+  getStudykitIdFromRout,
+  randomizeStudycards,
+} from "../../services/_utils/helper";
+import { DataService } from "../..//services/data.service";
+
+import { Card } from "../../services/_interfaces/card";
+import { Studykit } from "../../services/_interfaces/studykit";
 
 @Component({
   selector: "app-read-studycard",
@@ -35,9 +41,9 @@ export class ReadStudycardPage implements OnInit {
     cards: this.studycards,
   };
 
-  indexOfUserCard: number = 0;
-  cardIndex: number = 0;
-  cardSortIndexes: number[] = [];
+  // values to describe shown cards
+  cardIndex: number = 0; // index of card of card, which the user sees
+  studycardArray: Card[] = []; // studycards of studykit
   cardToShow: Card = {
     id: "",
     lastLearnedOn: new Date(),
@@ -46,58 +52,48 @@ export class ReadStudycardPage implements OnInit {
     type: "",
     question: "",
     answers: [],
-  };
+  }; // current shown card
 
   ngOnInit() {}
 
   async ionViewWillEnter() {
-    const routeParamStudykitId = this.route.snapshot.paramMap.get("id");
-    if (routeParamStudykitId !== null) {
-      this.studykit = await this.service.getStudykitById(routeParamStudykitId);
-      this.cardSortIndexes = this.generateRandomNumber();
-      this.cardIndex = this.cardSortIndexes[this.indexOfUserCard];
-      this.cardToShow = this.studykit.cards[this.cardIndex];
+    const routeParam: string = getStudykitIdFromRout(this.route);
+    if (routeParam !== null) {
+      this.initLearning(routeParam);
     }
   }
 
+  /* -------------------------------------- */
+  /* init */
+  /* -------------------------------------- */
+  /**
+   * Prepare studykit for learning
+   */
+  async initLearning(studykitId: string) {
+    this.studykit = await this.service.getStudykitById(studykitId);
+    this.studycardArray = randomizeStudycards(this.studykit.cards);
+    this.cardToShow = this.studycardArray[this.cardIndex];
+  }
+
+  /**
+   * Show next card
+   */
   increaseCardIndex() {
-    if (this.indexOfUserCard < this.studykit.cards.length - 1) {
-      this.indexOfUserCard++;
-      this.cardIndex = this.cardSortIndexes[this.indexOfUserCard];
-      this.cardToShow = this.studykit.cards[this.cardIndex];
+    if (this.cardIndex < this.studycardArray.length - 1) {
+      this.cardIndex++;
+      this.cardToShow = this.studycardArray[this.cardIndex];
     } else {
       this.presentToast("bottom", "success", "Du hast alle Lernkarten von diesem Set gelernt");
       this.router.navigate(['/home'])
     }
   }
+
+  /**
+   * Show previous card
+   */
   decreaseCardIndex() {
-    this.indexOfUserCard--;
-    this.cardIndex = this.cardSortIndexes[this.indexOfUserCard];
-    this.cardToShow = this.studykit.cards[this.cardIndex];
-  }
-
-  generateRandomNumber() {
-    let arrayOfNumbersLengthStudykitCards: number[] = this.generateNumbers();
-    return this.generateRandomSequence(arrayOfNumbersLengthStudykitCards);
-  }
-  generateNumbers(): number[] {
-    let result: number[] = [];
-    for (let i = 0; i < this.studykit.cards.length; i++) {
-      result.push(i);
-    }
-    return result;
-  }
-  generateRandomSequence(array: number[]): number[] {
-    let arrayToRandomize: number[] = array;
-    let result: number[] = [];
-
-    while (arrayToRandomize.length !== 0) {
-      let index = Math.floor(Math.random() * arrayToRandomize.length);
-      result.push(arrayToRandomize[index]);
-      arrayToRandomize.splice(index, 1);
-    }
-
-    return result;
+    this.cardIndex--;
+    this.cardToShow = this.studycardArray[this.cardIndex];
   }
 
   /**
