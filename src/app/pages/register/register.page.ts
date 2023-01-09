437485c69f0cb30ea09595ecedbbfc537540a7f9
Michelle Markl <65341595+MMischi@users.noreply.github.com>
2023-01-09 17:58:28 +0100

    feat: adding logic for registration

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,20 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.page.html',
-  styleUrls: ['./register.page.scss'],
+  selector: "app-register",
+  templateUrl: "./register.page.html",
+  styleUrls: ["./register.page.scss"],
 })
 export class RegisterPage implements OnInit {
+  
+  credentials: FormGroup;
 
-  user_nickname: string;
-  user_email: string;
-  user_password: string;
-  user_password_secure: string;
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  get email() {
+    return this.credentials.get("email");
   }
 
+  get password() {
+    return this.credentials.get("password");
+  }
+
+  ngOnInit() {
+    this.credentials = this.fb.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  async register() {
+    const user = await this.authService.register(this.credentials.value);
+
+    if (user) {
+      // authorisation successfull
+      this.router.navigateByUrl("/home", { replaceUrl: true });
+    } else { 
+      // TODO: error handling
+      console.error('something went wrong');
+    }
+  }
 }
