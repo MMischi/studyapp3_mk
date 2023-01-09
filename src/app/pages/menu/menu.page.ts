03ef92d0ac70190b7386b0d987d12d5d2f0afb20
Michelle Markl <65341595+MMischi@users.noreply.github.com>
2023-01-09 18:06:26 +0100

    feat: adding logic for logging out

--- src/app/pages/menu/menu.page.ts
+++ src/app/pages/menu/menu.page.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.page.html',
-  styleUrls: ['./menu.page.scss'],
+  selector: "app-menu",
+  templateUrl: "./menu.page.html",
+  styleUrls: ["./menu.page.scss"],
 })
 export class MenuPage implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  async logout() {
+    this.authService.logout();
+    this.router.navigateByUrl("login", { replaceUrl: true });
   }
-
 }
