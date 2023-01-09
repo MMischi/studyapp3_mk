b85fe576ba1c69dfc61d344aafef4e8e8e9133e4
Michelle Markl <65341595+MMischi@users.noreply.github.com>
2023-01-09 17:14:53 +0100

    feat: adding redirection depending on login state

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+import {
+  redirectUnauthorizedTo, 
+  redirectLoggedInTo,
+  canActivate
+} from '@angular/fire/auth-guard'
+
+const redirectUnauthorizedToRegister = () => redirectUnauthorizedTo(['register']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['home']);
+
+
 const routes: Routes = [
   {
     path: '',
@@ -9,43 +19,53 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule),
+    ...canActivate(redirectUnauthorizedToRegister),
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule),
+    ...canActivate(redirectLoggedInToHome),
   },
   {
     path: 'register',
-    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule),
+    ...canActivate(redirectLoggedInToHome),
   },
   {
     path: 'create-studykit',
-    loadChildren: () => import('./pages/create-studykit/create-studykit.module').then( m => m.CreateStudykitPageModule)
+    loadChildren: () => import('./pages/create-studykit/create-studykit.module').then( m => m.CreateStudykitPageModule),
+    ...canActivate(redirectUnauthorizedToRegister),
   },
   {
     path: 'edit-studykit/:id',
-    loadChildren: () => import('./pages/create-studykit/create-studykit.module').then( m => m.CreateStudykitPageModule)
+    loadChildren: () => import('./pages/create-studykit/create-studykit.module').then( m => m.CreateStudykitPageModule),
+    ...canActivate(redirectUnauthorizedToRegister),
   },
   {
     path: 'menu',
-    loadChildren: () => import('./pages/menu/menu.module').then( m => m.MenuPageModule)
+    loadChildren: () => import('./pages/menu/menu.module').then( m => m.MenuPageModule),
+    ...canActivate(redirectUnauthorizedToRegister),
   },
   {
     path: 'studykit-overview',
-    loadChildren: () => import('./pages/studykit-overview/studykit-overview.module').then( m => m.StudykitOverviewPageModule)
+    loadChildren: () => import('./pages/studykit-overview/studykit-overview.module').then( m => m.StudykitOverviewPageModule),
+    ...canActivate(redirectUnauthorizedToRegister),
   },
   {
     path: 'read-studycard/:id',
-    loadChildren: () => import('./pages/read-studycard/read-studycard.module').then( m => m.ReadStudycardPageModule)
+    loadChildren: () => import('./pages/read-studycard/read-studycard.module').then( m => m.ReadStudycardPageModule),
+    ...canActivate(redirectUnauthorizedToRegister),
   },
   {
     path: 'learn-studycard/:id',
-    loadChildren: () => import('./pages/learn-studycard/learn-studycard.module').then( m => m.LearnStudycardPageModule)
+    loadChildren: () => import('./pages/learn-studycard/learn-studycard.module').then( m => m.LearnStudycardPageModule),
+    ...canActivate(redirectUnauthorizedToRegister),
   },
   {
     path: 'quiz-studycard/:id',
-    loadChildren: () => import('./pages/quiz-studycard/quiz-studycard.module').then( m => m.QuizStudycardPageModule)
+    loadChildren: () => import('./pages/quiz-studycard/quiz-studycard.module').then( m => m.QuizStudycardPageModule),
+    ...canActivate(redirectUnauthorizedToRegister),
   },
 ];
 
