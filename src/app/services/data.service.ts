0c27efc8e294d158d37ddfeeccd49da3d53a10ba
Michelle Markl <65341595+MMischi@users.noreply.github.com>
2023-01-13 16:27:06 +0100

    refactor: prettier formatter

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,32 +1,31 @@
-import { Injectable } from '@angular/core';
-import { Storage } from '@ionic/storage-angular';
-import { Card } from './_interfaces/card';
-import { Studykit } from './_interfaces/studykit';
+import { Injectable } from "@angular/core";
+import { Storage } from "@ionic/storage-angular";
+import { Card } from "./_interfaces/card";
+import { Studykit } from "./_interfaces/studykit";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class DataService {
-
-  private STUDYKIT_DATA_STORAGE = 'studykits';
+  private STUDYKIT_DATA_STORAGE = "studykits";
 
   constructor(private storage: Storage) {
     this.init();
   }
-  
+
   async init() {
     await this.storage.create();
   }
 
   /**
-   * retruns data from local storage by key 
-   * 
+   * retruns data from local storage by key
+   *
    * @param {string} key - name of storage key
-   * @returns content of storage in key or empty array 
+   * @returns content of storage in key or empty array
    */
   async getData(key: string) {
     return (await this.storage.get(key)) || [];
-  } 
+  }
 
   // local storage: studykit endpoints
   /**
@@ -44,12 +43,12 @@ export class DataService {
    */
   async getStudykitById(studykitId: string): Promise<Studykit> {
     let allStudykits = await this.getAllStudykits();
-    return allStudykits.filter((elem: Studykit) => elem.id === studykitId)[0]
+    return allStudykits.filter((elem: Studykit) => elem.id === studykitId)[0];
   }
 
   /**
    * store one studykit to local storage
-   * @param {Studykit} studykit 
+   * @param {Studykit} studykit
    */
   async storeStudykit(studykit: Studykit) {
     let allStudykits = await this.getAllStudykits();
@@ -63,39 +62,47 @@ export class DataService {
    */
   async deleteStudykit(studykitId: string) {
     let allStudykits = await this.getAllStudykits();
-    let indexOfStudykit = allStudykits.findIndex((elem: Studykit) => elem.id === studykitId);
+    let indexOfStudykit = allStudykits.findIndex(
+      (elem: Studykit) => elem.id === studykitId
+    );
     allStudykits.splice(indexOfStudykit, 1);
     this.saveStudykits(allStudykits);
   }
 
   /**
-   * update a studykit 
+   * update a studykit
    * @param {Studykit} studykit - full studykit
    */
   async updateStudykit(studykit: Studykit) {
     let allStudykits = await this.getAllStudykits();
-    let indexOfStudykit = allStudykits.findIndex((elem: Studykit) => elem.id === studykit.id);
+    let indexOfStudykit = allStudykits.findIndex(
+      (elem: Studykit) => elem.id === studykit.id
+    );
     allStudykits[indexOfStudykit] = studykit;
     this.saveStudykits(allStudykits);
   }
 
   /**
    * save all studykits
-   * @param studykits 
+   * @param studykits
    */
-   async saveStudykits(studykits: Studykit[]) {
+  async saveStudykits(studykits: Studykit[]) {
     this.storage.set(this.STUDYKIT_DATA_STORAGE, studykits);
   }
 
   /**
    * updates card in studykit by id
-   * @param { string } studykitId 
-   * @param { Card } card 
+   * @param { string } studykitId
+   * @param { Card } card
    */
   async updateCardInStudykit(studykitId: string, card: Card) {
     let allStudykits = await this.getAllStudykits();
-    let indexOfStudykit = allStudykits.findIndex((elem: Studykit) => elem.id === studykitId);
-    let indexOfCard = allStudykits[indexOfStudykit].cards.findIndex((elem: Card) => elem.id === card.id);
+    let indexOfStudykit = allStudykits.findIndex(
+      (elem: Studykit) => elem.id === studykitId
+    );
+    let indexOfCard = allStudykits[indexOfStudykit].cards.findIndex(
+      (elem: Card) => elem.id === card.id
+    );
     allStudykits[indexOfStudykit].cards[indexOfCard] = card;
     this.saveStudykits(allStudykits);
   }
