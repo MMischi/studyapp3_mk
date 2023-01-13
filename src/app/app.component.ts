78e477edd8f4f533bbc596d99c5fd8831312d5fe
Michelle Markl <65341595+MMischi@users.noreply.github.com>
2023-01-13 16:13:00 +0100

    feat: info if user is offline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { ToastController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private toastController: ToastController) {
+    window.addEventListener("offline", () => {
+      this.presentToast(
+        "bottom",
+        "warning",
+        "Dein Smartphone ist offline. Daten werden vorläufig nur lokal gespeichert."
+      );
+    });
+
+    window.addEventListener("online", () => {
+      this.presentToast("bottom", "success", "Dein Smartphone ist online.");
+    });
+  }
+
+  ngOnInit() {
+    navigator.onLine
+      ? ""
+      : this.presentToast(
+          "bottom",
+          "warning",
+          "Dein Smartphone ist offline. Daten werden vorläufig nur lokal gespeichert."
+        );
+  }
+
+  /**
+   * show toast
+   * @param {string} position - "top" | "middle" | "bottom"
+   * @param {string} color - "danger" | "warning" | "primary" | "light" | "success"
+   * @param {string} msg - individual
+   */
+  async presentToast(
+    position: "top" | "middle" | "bottom",
+    color: "danger" | "warning" | "primary" | "light" | "success",
+    msg: string
+  ) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000,
+      position: position,
+      color: color,
+    });
+
+    await toast.present();
+  }
 }
