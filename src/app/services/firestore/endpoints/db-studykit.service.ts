623573a62f236c9ac05428006bd895449d70bb8b
Michelle Markl <65341595+MMischi@users.noreply.github.com>
2023-01-13 16:26:28 +0100

    feat: adding rest requests

--- src/app/services/firestore/endpoints/db-studykit.service.ts
+++ src/app/services/firestore/endpoints/db-studykit.service.ts
@@ -1,17 +1,24 @@
-import { Observable } from 'rxjs';
-import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+import { Injectable } from "@angular/core";
 
-import { addDoc, collection, collectionData, Firestore } from '@angular/fire/firestore';
-import { Studykit } from '../../_interfaces/studykit';
+import {
+  Firestore,
+  doc,
+  collection,
+  collectionData,
+  updateDoc,
+  addDoc,
+  deleteDoc,
+} from "@angular/fire/firestore";
+import { Studykit } from "../../_interfaces/studykit";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class DbStudykitService {
-  
-  private STUDYKIT_DATA_STORAGE = 'studykits';
+  private STUDYKIT_DATA_STORAGE = "studykits";
 
-  constructor(private firestore: Firestore) { }
+  constructor(private firestore: Firestore) {}
 
   /**
    * creates reference to collection
@@ -21,14 +28,20 @@ export class DbStudykitService {
     return collection(this.firestore, this.STUDYKIT_DATA_STORAGE);
   }
 
+  getStudykitByIdFromDb(id: string) {
+    return doc(this.firestore, `${this.STUDYKIT_DATA_STORAGE}/${id}`);
+  }
+
   /**
    * retruns data from db storage by key (including id of object)
-   * 
+   *
    * @returns content of studykit storage
    */
   getAllStudykitsDB(): Observable<Studykit[]> {
-    return collectionData(this.getStudykitCollection(), {idField: 'id'}) as Observable<Studykit[]>;
-  } 
+    return collectionData(this.getStudykitCollection(), {
+      idField: "id",
+    }) as Observable<Studykit[]>;
+  }
 
   /**
    * store studykit to db
@@ -36,4 +49,21 @@ export class DbStudykitService {
   storeStudykitDB(studykit: Studykit) {
     addDoc(this.getStudykitCollection(), studykit);
   }
+
+  /**
+   * delete studykit by id from db
+   * @param {string} studykitId - id of studykit
+   */
+  async deleteStudykit(studykitId: string) {
+    deleteDoc(this.getStudykitByIdFromDb(studykitId));
+  }
+
+  /**
+   * update a studykit on db
+   * @param {Studykit} studykit - full studykit
+   */
+  updateStudykitDB(studykit: Studykit) {
+    updateDoc(this.getStudykitByIdFromDb(studykit.id), { studykit });
+  }
+  
 }
