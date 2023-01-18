1d136cf5de80e60e819437011a88ab5e5df0f6f2
Michelle Markl <65341595+MMischi@users.noreply.github.com>
2023-01-18 10:32:27 +0100

    feat: adding icon and splash screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,28 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'study_app',
-  webDir: 'www',
-  bundledWebRuntime: false
+  appId: "studyapp_mk3",
+  appName: "StudyApp",
+  webDir: "www",
+  bundledWebRuntime: false,
+
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#7895ac",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#d8e8f5",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
